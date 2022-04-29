c250a54369ea4f1e76368800726cf1af538e3790
Marcin Kubica <marcin.kubica@engineerbetter.com>
2022-04-29 11:40:03 +0100

    vpc back to region
    Signed-off-by: William Young <will.young@engineerbetter.com>

--- terraform/gke/main.tf
+++ terraform/gke/main.tf
@@ -50,7 +50,7 @@ resource "google_compute_network" "vpc" {
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
   name          = "${var.project}-subnet"
-  region        = var.zone
+  region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
 }
