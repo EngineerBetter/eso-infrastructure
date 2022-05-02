4ddf4b682df956dfe5737005faa65b93efa510de
Gustavo Carvalho <gustavo.carvalho@container-solutions.com>
2022-05-02 06:56:06 -0300

    Added missing project field
    Signed-off-by: Gustavo Carvalho <gustavo.carvalho@container-solutions.com>

--- terraform/project/acr.tf
+++ terraform/project/acr.tf
@@ -1,7 +1,7 @@
 
 resource "google_artifact_registry_repository" "eso" {
   provider = google-beta
-
+  project = var.project
   location = var.region
   repository_id = "eso"
   description = "repository for eso related images"
