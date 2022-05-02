bf351b10f9db9c0f82525618fd992c4785e2ecf5
Gustavo Carvalho <gustavo.carvalho@container-solutions.com>
2022-05-02 06:57:51 -0300

    Changing ACR name to a valid one
    Signed-off-by: Gustavo Carvalho <gustavo.carvalho@container-solutions.com>

--- terraform/project/acr.tf
+++ terraform/project/acr.tf
@@ -3,7 +3,7 @@ resource "google_artifact_registry_repository" "eso" {
   provider = google-beta
   project = var.project
   location = var.region
-  repository_id = "eso"
+  repository_id = "beach-team-eso"
   description = "repository for eso related images"
   format = "DOCKER"
 }
