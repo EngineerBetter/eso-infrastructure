ac9c91928f56d150beaa868bf856bf565f10bace
Gustavo Carvalho <gustavo.carvalho@container-solutions.com>
2022-05-02 06:40:06 -0300

    Added missing API
    Signed-off-by: Gustavo Carvalho <gustavo.carvalho@container-solutions.com>

--- terraform/gke/main.tf
+++ terraform/gke/main.tf
@@ -72,3 +72,9 @@ resource "google_project_service" "container" {
   service = "container.googleapis.com"
   disable_on_destroy = false
 }
+
+resource "google_project_service" "artifactregistry" {
+  service = "artifactregistry.googleapis.com"
+  disable_on_destroy = false
+}
+
