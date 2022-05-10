e9cea6e775fc6da42a707552d27866f2014873eb
Marcin Kubica <marcin.kubica@engineerbetter.com>
2022-05-10 14:43:01 +0100

    Fix validation-tests (with some nice debug info)
    Add fly trigger script for test-build-deploy pipeline group.
    
    Signed-off-by: William Young <will.young@engineerbetter.com>
    Signed-off-by: Dominic Meddick <dom.meddick@engineerbetter.com>

---
+++ scripts/build-pipeline-trigger.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+TARGET=$(fly targets |grep "ci\.engineerbetter\.com" |awk '{print $1}')
+TEAM="kubernetes-eso"
+#TODO: have this read from pipeline config
+JOBS="lint unit-tests build deploy validation-tests"
+
+#TODO: detect from ../workspace/external-secrets branch
+PIPELINE="eso-cd"
+
+Echo "Executing fly trigger job for target $TARGET and pipeline $PIPELINE"
+fly -t $TARGET check-resource --team=$TEAM -r $PIPELINE/external-secrets-repo
+
+for JOB in $JOBS; do
+  fly -t $TARGET trigger-job --team=$TEAM -j $PIPELINE/$JOB -w
+done
