8bca132f0e4bc35cb2781baaaea438a4f929d2a2
amr fawzy <amrfawzymoh@gmail.com>
2022-06-22 12:24:30 +0200

    Disable vault HA, enable default stand alone mode
    Signed-off-by: Amr Fawzy <amr.fawzy@container-solutions.com>

--- terraform/vault/vault-helm.tf
+++ terraform/vault/vault-helm.tf
@@ -1,15 +1,7 @@
 resource "helm_release" "vault-release" {
   name = "vault"
-  namespace  = var.vault-namespace
-  create_namespace = true
   repository = var.vault-helm-repo
   chart      = var.vault-helm-chart
-  set {
-    name  = "server.ha.enabled"
-    value = "true"
-  }
-  set {
-    name  = "server.ha.raft.enabled"
-    value = "true"
-  }
+  namespace  = var.vault-namespace
+  create_namespace = true
 }
